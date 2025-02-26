1c68390b99b56212a607cda8456602a5eaa2d55a
alaqs <ddrrooppeer@gmail.com>
2025-02-26 11:17:00 +0500

    Create bat file for start project.

--- start_project.bat
+++ start_project.bat
@@ -1,12 +1,12 @@
 @echo off
 cd /d C:\Users\alaqs\projecttireshop
 
-echo Запуск сервера...
+echo Запуск сервера....
 start cmd /k "node server.js"
 
 timeout /t 5
 
-echo Запуск клиента...
+echo Запуск клиента....
 start cmd /k "cd client && npm start"
 
 echo Все службы запущены!
